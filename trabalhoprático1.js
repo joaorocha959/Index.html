4f32f1e7ccb4b23f2b5f495e4b54d2d502cd406c
joaorocha959 <joaorocha959@gmail.com>
2025-07-02 00:16:28 -0300

    Delete trabalhoprático1.js

--- trabalhoprático1.js
+++
@@ -1,9 +0,0 @@
-let botao = document.querySelector("#botao");
-botao.style.background="blue";
-
-botao.addEventListener("click",e =>{
-botao.style.background="yellow";
-botao.innerHTML="Faça sempre o seu melhor!";
-
-});
-

1805ffbbca6628e7bebe421e30cb88839f701082
joaorocha959 <joaorocha959@gmail.com>
2025-07-02 00:45:20 -0300

    Add files via upload

---
+++ Index.html.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector("#botao");
+botao.style.background="blue";
+
+botao.addEventListener("click",e =>{
+botao.style.background="yellow";
+botao.innerHTML="Faça sempre o seu melhor!";
+
+});
+
